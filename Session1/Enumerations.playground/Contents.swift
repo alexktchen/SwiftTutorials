8da29bcffdb472548adab05812f9a89ff3f42656
Alex Chen <alexc@bsquare.com>
2016-03-19 16:11:24 +0800

    Add Enumeration

--- Session1/Enumerations.playground/Contents.swift
+++ Session1/Enumerations.playground/Contents.swift
@@ -2,4 +2,55 @@
 
 import UIKit
 
-var str = "Hello, playground"
+// Enumeration Syntax
+
+
+enum UserType {
+    case student
+    case engineer
+    case accounting
+    case administration
+}
+
+
+let user1 = UserType.administration
+let user2: UserType = .engineer
+
+
+// with values
+enum UserTypeWhitValue {
+    case administration(String, String)
+}
+
+let user3 = UserTypeWhitValue.administration("123", "456")
+
+switch user3 {
+case .administration("111","222") :
+    print("incorrect")
+case .administration("123","456"):
+    print("correct")
+default:
+    print("default")
+}
+
+
+// Raw Values
+enum UserTypeWithRawValues: String {
+    case student = "Eric"
+    case engineer = "Alex"
+    case accounting = "Vicky"
+    case administration = "Daisy"
+}
+
+let user4 = UserTypeWithRawValues.administration.rawValue
+let user5 = UserTypeWithRawValues.engineer.rawValue
+
+let user6 = UserTypeWithRawValues(rawValue: "Alex")
+let user7 = UserTypeWithRawValues(rawValue: "Tracy")
+
+
+
+
+
+
+
